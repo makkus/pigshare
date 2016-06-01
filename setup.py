d1777039bdb39bc925c275a8dfa14e4998bace4e
Markus Binsteiner <makkus@gmail.com>
2016-06-01 16:17:43 +1200

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,6 @@ init_py = io.open('pigshare/__init__.py').read()
 metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", init_py))
 metadata['doc'] = re.findall('"""(.+)"""', init_py)[0]
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read().replace('.. :changelog:', '')
-
 requirements = [
     "argparse",
     "setuptools",
@@ -55,9 +49,6 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
     ],
     test_suite='tests',
     tests_require=test_requirements,

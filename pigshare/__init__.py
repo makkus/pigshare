ca48bb65526a79dfe1968718e85bee861bdf0092
Markus Binsteiner <makkus@gmail.com>
2016-07-05 09:45:30 +1200

    Version stuff

--- pigshare/__init__.py
+++ pigshare/__init__.py
@@ -3,7 +3,7 @@
 """Python client for figshare"""
 
 
-__version__ = '0.5.3'
+__version__ = '0.5.0'
 __url__ = 'https://github.com/makkus/pigshare'
 __author__ = 'Markus Binsteiner'
 __email__ = 'makkus@gmail.com'
